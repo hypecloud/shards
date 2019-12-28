ed7fa21dc5c3522fdfcc327804610e2f8b32c2a6
Tomek Marciniak <tmarciniakm@gmail.com>
2019-12-28 09:06:16 +0100

    feat(crud): implement missing crud methods

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,36 +1,15 @@
-import dayjs from 'dayjs'
-import { MongoClient, ObjectId } from 'mongodb'
 import { getCollections } from '../lib/db'
-import { generateCollections, generateQueries } from '../lib/resolvers'
+import { generateQueries, generateMutations } from '../lib/resolvers'
 import shardsConfig from '../shards.config'
 
 const models = shardsConfig.models
-
 const collections = getCollections(models)
-console.log(collections)
 
 const resolvers = () => {
   return {
     Query: generateQueries(models, collections),
-    Mutation: {
-      async createRealm(_, args) {
-        args.updatedAt = dayjs().toISOString()
-        const { ops: [first] } = await collections.realms.insertOne(args)
-        return first
-      },
-      async updateRealm(_, { id, ...updateParams }) {
-        updateParams.updatedAt = dayjs().toISOString()
-        const { value } = await collections.realms.findOneAndUpdate(
-          { _id: ObjectId(id) },
-          { $set: updateParams },
-          { returnOriginal: false }
-        )
-        return value
-      }
-    }
+    Mutation: generateMutations(models, collections)
   }
 }
-
-console.log('res', resolvers())
 
 export default resolvers
